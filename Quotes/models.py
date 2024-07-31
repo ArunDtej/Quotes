ab97f7658e45fb2868d10da9c717d0fe51c52cd2
arun.t <arun.t@bluecloudsoftech.com>
2024-07-31 18:21:45 +0000

    j1

--- Quotes/models.py
+++ Quotes/models.py
@@ -46,7 +46,9 @@ class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name= 'notifications')
     Type = models.CharField(max_length=255)
     from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_from')
-    created_at = models.DateTimeField(auto_now_add=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    post_id = models.IntegerField(blank=True, null=True)
+    is_read = models.BooleanField(default=False)
 
     def __str__(self):
         return self.Type
